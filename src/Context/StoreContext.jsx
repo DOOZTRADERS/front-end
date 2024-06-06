3a433206e29d75a5c985e2e58dc543bc97201a2b
DOOZTRADERS <134775031+DOOZTRADERS@users.noreply.github.com>
2024-06-06 19:08:14 +0530

    Update StoreContext.jsx

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = "https://back-end-6-pzkg.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
