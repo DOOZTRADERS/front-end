c94ecc672d599bd4d5be9810867cef106e5c16a8
DOOZTRADERS <134775031+DOOZTRADERS@users.noreply.github.com>
2024-06-07 17:25:29 +0530

    Update AppDownload.jsx

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -5,10 +5,11 @@ import { assets } from '../../assets/assets'
 const AppDownload = () => {
     return (
         <div className='app-download' id='app-download'>
-            <p>For Better Experience Download <br />Tomato App</p>
+            <p>For Better Experience Download <br/>Madhira Food Delivery App</p>
             <div className="app-download-platforms">
                 <img src={assets.play_store} alt="" />
                 <img src={assets.app_store} alt="" />
+              <p> Coming Soon </p>   
             </div>
         </div>
     )
